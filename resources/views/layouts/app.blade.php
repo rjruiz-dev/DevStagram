594d0d8028ef513e8a5aa54ed35bd1476c2f4e4b
rjr <rjruizsf@gmail.com>
2023-02-27 03:41:10 -0300

    creando formulario de registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
         </main>
 
         {{-- p-5 padding --}}
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
             DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>
         
