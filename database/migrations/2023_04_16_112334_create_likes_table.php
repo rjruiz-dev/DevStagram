6dab4ce4b60b46be9f423c03fde91182c13fbb34
rjr <rjruizsf@gmail.com>
2023-04-16 08:31:35 -0300

    Creando modelo migracion controller Like

---
+++ database/migrations/2023_04_16_112334_create_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /**
+         * Guarda la referencia en latabla de likes el usuario y al posts que le dio like y de esa forma poder
+         * contar cuantos likes hay y tambien si esa persona ya le dio like a una publicacion o no 
+         */
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
