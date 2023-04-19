547a786bef86f979536023d645d3316d92f79268
rjr <rjruizsf@gmail.com>
2023-04-18 21:33:36 -0300

    Mostrar la cantidad de likes que tiene una publicacion

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -43,7 +43,9 @@
                     @endif     
                 @endauth
 
-                <p>0 Likes</p>
+                <p class="font-bold">{{ $post->likes->count() }} 
+                   <span class="font-normal"> Likes </span>
+                </p>
             </div>
 
             <div>
