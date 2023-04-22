5dbaf469df661f645af16553ee60f345ca70d6f7
rjr <rjruizsf@gmail.com>
2023-04-22 19:19:39 -0300

    Creando formulario para editar perfil

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -5,5 +5,55 @@
 @endsection
 
 @section('contenido')
-    Contenido
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form action="" class="mt-10 md:mt-0">
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input 
+                        id="username"
+                        name="username" 
+                        type="text"
+                        placeholder="Tu Nombre de Usuario" 
+                        {{--  border gris, p padding de 3, w-full toma todo el ancho disp, esq redondeadas  --}}
+                        class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
+                        {{-- mantiene el valor ingresado --}}
+                        value="{{ auth()->user()->username }}"
+                    >
+
+                    {{-- el error  --}}
+                    @error('username')
+                        {{-- formato y color al error --}}
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Imagen Perfil
+                    </label>
+                    <input 
+                        id="imagen"
+                        name="imagen" 
+                        type="file"                    
+                        class="border p-3 w-full rounded-lg"                     
+                        value=""
+                        accept=".jpg, .jpeg, .png"
+                    >
+                </div>
+
+                <input 
+                    type="submit"
+                    value="Guardar Cambios"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg
+                    "
+                >
+
+            </form>
+        </div>
+    </div>
 @endsection
