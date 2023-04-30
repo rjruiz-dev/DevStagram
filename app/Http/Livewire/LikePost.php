833689f91d28623d96d5e0ccc506c21a198a7bc6
rjr <rjruizsf@gmail.com>
2023-04-30 19:37:19 -0300

    Mover la logica de likes y dejar de dar likes a livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -6,11 +6,21 @@ use Livewire\Component;
 
 class LikePost extends Component
 {
+    // Importante $request no esta diponible en livewire
     public $post;
 
     public function like() 
     {
-        return "Desde la funcion de like";
+        // Revisa si el usuario actual ya dio "me gusta"
+        if ( $this->post->checkLike(auth()->user())) {
+            // Eliminar like
+            $this->post->likes()->where('post_id', $this->post->id)->delete();           
+        }else {
+            // Guardar like
+            $this->post->likes()->create([
+                'user_id' => auth()->user()->id
+            ]);
+        }
     }
 
     public function render()
