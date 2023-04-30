3891f5e3db00d3aca68517789711109da5f658d0
rjr <rjruizsf@gmail.com>
2023-04-30 20:02:10 -0300

    Interfaces reactivas con livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -8,18 +8,29 @@ class LikePost extends Component
 {
     // Importante $request no esta diponible en livewire
     public $post;
+    public $isLiked;
+
+    // Saber cuando el usuario entra a la vista y detectar si le dio "me gusta"    
+    // Ciclo de vida mount se ejeucuta automaticamente cuando sea instanciado
+    public function mount($post)
+    {
+        $this->isLiked = $post->checkLike(auth()->user());
+    }
 
     public function like() 
     {
         // Revisa si el usuario actual ya dio "me gusta"
         if ( $this->post->checkLike(auth()->user())) {
             // Eliminar like
-            $this->post->likes()->where('post_id', $this->post->id)->delete();           
+            $this->post->likes()->where('post_id', $this->post->id)->delete(); 
+            // Re-render
+            $this->isLiked = false;           
         }else {
             // Guardar like
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
+            $this->isLiked = true;
         }
     }
 
