4a9ed5d86c285b843afc002bad2981901d51f610
rjr <rjruizsf@gmail.com>
2023-04-30 20:11:19 -0300

    Actualizar el contador de likes

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -9,12 +9,15 @@ class LikePost extends Component
     // Importante $request no esta diponible en livewire
     public $post;
     public $isLiked;
+    public $likes;
 
     // Saber cuando el usuario entra a la vista y detectar si le dio "me gusta"    
     // Ciclo de vida mount se ejeucuta automaticamente cuando sea instanciado
     public function mount($post)
     {
         $this->isLiked = $post->checkLike(auth()->user());
+        // una ves instanciado leemos la cantidad de like que tiene y la asignamos a this->likes
+        $this->likes = $post->likes->count();
     }
 
     public function like() 
@@ -24,13 +27,15 @@ class LikePost extends Component
             // Eliminar like
             $this->post->likes()->where('post_id', $this->post->id)->delete(); 
             // Re-render
-            $this->isLiked = false;           
+            $this->isLiked = false;     
+            $this->likes--;  
         }else {
             // Guardar like
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
             $this->isLiked = true;
+            $this->likes++;
         }
     }
 
