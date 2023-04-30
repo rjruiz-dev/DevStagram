33f52570e433ecd9427f89ff8bf9486ef4ca5afa
rjr <rjruizsf@gmail.com>
2023-04-30 19:25:44 -0300

    Eventos en livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -8,6 +8,11 @@ class LikePost extends Component
 {
     public $post;
 
+    public function like() 
+    {
+        return "Desde la funcion de like";
+    }
+
     public function render()
     {
         return view('livewire.like-post');
